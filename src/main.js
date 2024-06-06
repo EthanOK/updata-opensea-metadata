c79ff31328811f176d8cf6e76262853e01cd9c21
EthanOK <sdzdj01@163.com>
2024-06-06 23:50:54 +0800

    add:   update metadata in opensea testnet

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-const { update_metadata } = require("./update_metadata");
+const {
+  update_opensea_metadata,
+  Chain,
+  update_opensea_metadata_testnet,
+} = require("./update_metadata");
 
 async function main() {
-  const start_tokenId = 1800;
-  const end_tokenId = 7200;
-  await update_metadata(
+  const start_tokenId = 714;
+  const end_tokenId = 714;
+  await update_opensea_metadata(
+    Chain.Mainnet,
     "0x1b489201D974D37DDd2FaF6756106a7651914A63",
+    start_tokenId,
+    end_tokenId
+  );
+
+  await update_opensea_metadata_testnet(
+    Chain.Sepolia,
+    "0x709b78b36b7208f668a3823c1d1992c0805e4f4d",
     start_tokenId,
     end_tokenId
   );
--- src/update_metadata.js
+++ src/update_metadata.js
@@ -1,14 +1,42 @@
 require("dotenv").config();
 const sdk = require("api")("@opensea/v2.0#4pzkm2xlrva0b3l");
+const { Chain } = require("opensea-js");
 const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY;
 
-async function update_metadata(contract_address, start_tokenId, end_tokenId) {
+async function update_opensea_metadata(
+  chain,
+  contract_address,
+  start_tokenId,
+  end_tokenId
+) {
   sdk.auth(OPENSEA_API_KEY);
   sdk.server("https://api.opensea.io");
   for (let i = start_tokenId; i <= end_tokenId; i++) {
     try {
       const result = await sdk.refresh_nft({
-        chain: "ethereum",
+        chain: chain,
+        address: contract_address,
+        identifier: i,
+      });
+      console.log(result.data);
+    } catch (error) {
+      console.log(error.data);
+    }
+  }
+}
+
+async function update_opensea_metadata_testnet(
+  chain,
+  contract_address,
+  start_tokenId,
+  end_tokenId
+) {
+  sdk.auth(OPENSEA_API_KEY);
+  sdk.server("https://testnets-api.opensea.io");
+  for (let i = start_tokenId; i <= end_tokenId; i++) {
+    try {
+      const result = await sdk.refresh_nft({
+        chain: chain,
         address: contract_address,
         identifier: i,
       });
@@ -49,4 +77,9 @@ async function update_okx_metadata(
   }
 }
 
-module.exports = { update_metadata, update_okx_metadata };
+module.exports = {
+  update_opensea_metadata,
+  update_opensea_metadata_testnet,
+  update_okx_metadata,
+  Chain,
+};
